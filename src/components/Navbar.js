e55896fca0a3b32befb46f462478da3a4698c2e1
shubhamkanse72 <shubhamkanse7272@gmail.com>
2024-07-22 19:07:17 +0530

    initial setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { connect, useDispatch } from "react-redux";
+import { changeTabActive } from "../redux/action.js";
+
+const Navbar = ({ activeTab }) => {
+  const [listNav] = useState(["home", "skills", "projects", "contacts"]);
+  const dispatch = useDispatch();
+
+  const changeTab = (value) => {
+    dispatch(changeTabActive(value));
+  };
+
+  return (
+    <header>
+      <div className="logo">{/* <img src='/logo.png'></img> */}</div>
+      <nav>
+        {listNav.map((value, key) => (
+          <span
+            className={activeTab === value ? "active" : ""}
+            key={key}
+            onClick={() => changeTab(value)}
+          >
+            {value}
+          </span>
+        ))}
+      </nav>
+    </header>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  activeTab: state.activeTab,
+});
+
+export default connect(mapStateToProps, changeTabActive)(Navbar);
