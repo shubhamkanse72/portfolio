f2488448756d901f13ac53b89f5c47074a39a9ac
shubhamkanse72 <shubhamkanse7272@gmail.com>
2024-07-22 23:41:38 +0530

    home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <section className="home">
+      <div className="content">
+        <div className="name">
+          MY NAME IS <span>SHUBHAM</span>
+        </div>
+        <div className="des">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, ut
+          enim. Esse, aspernatur beatae? Quasi dicta enim distinctio doloribus
+          autem et ad? Accusantium aliquam esse mollitia totam amet dolorem at.
+        </div>
+        <a href={`${process.env.PUBLIC_URL}/resume.pdf`}>Download Resume</a>
+      </div>
+      <div className="avatar">
+        <div className="card">
+          <img src="/youth_img.jpg" alt="youth" />
+          <div className="info">
+            <div>Frontend Developer</div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
