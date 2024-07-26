fbd0443fd0a65b57828ffcb8c049ea7a5e2e8d75
shubhamkanse72 <shubhamkanse7272@gmail.com>
2024-07-26 20:35:32 +0530

    warnings fixed

--- src/components/CustomHook.js
+++ src/components/CustomHook.js
@@ -1,34 +1,20 @@
-import { useEffect } from 'react' 
-import { useSelector } from 'react-redux';
+import { useEffect, useCallback } from "react";
+import { useSelector } from "react-redux";
 
 const CustomHook = (refTab = null) => {
-const scrollTab = refTab;
-//const divs = refList;
-const activeTab = useSelector(state => state.activeTab);
+  const scrollTab = refTab;
+  const activeTab = useSelector((state) => state.activeTab);
 
-useEffect(() => {
-    if(scrollTab.current.classList.contains(activeTab)){
-        const componentNode = scrollTab.current;
-        componentNode.scrollIntoView({ behavior: 'smooth' });
+  const scrollToMenu = useCallback(() => {
+    if (scrollTab.current && scrollTab.current.classList.contains(activeTab)) {
+      const componentNode = scrollTab.current;
+      componentNode.scrollIntoView({ behavior: "smooth" });
     }
-    // if(divs !== null){
-    //     divs.current.forEach((div) => {
-    //         div.classList.add('animation');
-    //       });
-    //     const handlScroll = () => {
-    //       const scrollPosition = window.scrollY;
-    //       divs.current.forEach((div) => {
-    //         const offsetTop = div.getBoundingClientRect().top + scrollPosition;
-    //         if (scrollPosition >= offsetTop - (window.innerHeight / (1.5))) {
-    //           div.classList.add('active');
-    //         }else{
-    //           div.classList.remove('active');
-    //         }
-    //       });
-    //     }
-    //       window.addEventListener('scroll', handlScroll);
-    // }
-}, [activeTab])
-}
+  }, [scrollTab, activeTab]);
 
-export default CustomHook
+  useEffect(() => {
+    scrollToMenu();
+  }, [activeTab, scrollToMenu]);
+};
+
+export default CustomHook;
